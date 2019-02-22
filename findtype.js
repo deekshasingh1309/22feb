0ea43106eb2e69e6d17877579981d1803ed9d6cd
deekshasingh1309 <47428380+deekshasingh1309@users.noreply.github.com>
2019-02-22 18:28:38 +0530

    Add files via upload

---
+++ findtype.js
@@ -0,0 +1,65 @@
+
+function dataTypeCheck(value){
+
+    if (typeof value === 'string' || value instanceof String) { // Returns if a value is a string
+
+        console.log('Type : String');
+        console.log('value: ' + value);
+    }
+
+    else if (typeof value === 'number' && isFinite(value)) {  // Returns if a value is really a number
+        console.log('Type : Number');
+        console.log('value: ' + value);
+    }
+
+    else if (value === null) { // Returns if a value is null
+
+        console.log('Type : NULL')
+        console.log('value: ' + value)
+    }
+
+    else if (typeof value === 'undefined') {  // Returns if a value is undefined
+        console.log('Type : Undefined');
+    }
+
+    else if (typeof value === 'boolean') {  // Returns if a value is a boolean
+        console.log('Type : Boolean');
+    }
+
+    else if (value && typeof value === 'object' && value.constructor === Object) { // Returns if a value is an object
+        console.log('Type : Object')
+        console.log('value: ')
+        console.log(value);
+}
+
+    else if (typeof (value) === 'object' && typeof (value[0]) === 'object') { // Returns if a value is an array of object
+    console.log("Array of object");
+        value.map((x,i)=>{
+        console.log(x);
+})
+
+}
+
+    else if (typeof (value) === 'object' && typeof (value[0]) !== 'object') { // Returns if a value is an array
+    console.log("Type : Array");
+    value.map((x, i) => {
+        console.log(x);
+    })
+}
+
+    else if(Number.isNaN(value)){
+
+        console.log('type:NaN');
+
+    }
+    
+}
+
+dataTypeCheck("hi");
+dataTypeCheck(112);
+dataTypeCheck(null)
+var arr = [11, 22, 33]
+dataTypeCheck(arr)
+var arr_of_obj = [{ 'a': 45 }, { 'b' : 52 }]
+dataTypeCheck(arr_of_obj)
+dataTypeCheck({ 'c' : 2 })
